1f92f439cd923fadd4024181c2d4772ae935f618
enewhall <ewnewhal@gmail.com>
2015-02-10 13:04:59 -0500

    newer code for motor lab

--- Labs/Motor Lab/sketch_feb09a/sketch_feb09a.ino
+++ Labs/Motor Lab/sketch_feb09a/sketch_feb09a.ino
@@ -1,11 +1,11 @@
 int potPin = A1;
 float potAngle = 0.0;
 
-int encoderAPin = 2;
-int encoderBpin = 3;
+int encoderAPin = A4;
+int encoderBPin = A5;
 int encoderAValue = 0;
-int encoderAprev = 0;
-int encoderB = 0;
+int encoderAPrev = 0;
+int encoderBValue = 0;
 int encoderBPrev = 0;
 
 int DCMotorAngle = 0;
@@ -20,62 +20,107 @@ int toggle = 0;
 float stepDistance = 0.0;
 
 int limitSwitchPin = 4;
-int state = 1;
 
 int flexPin = A2;
 int servoPin = A3;
 
+const int OFF_STATE = 0;
+const int SERVO_STATE = 1;
+const int DC_STATE = 2;
+const int STEP_STATE = 3;
+
+int state = OFF_STATE;
+
+int button = HIGH;
+
 void setup() {
    Serial.begin(9600); 
+   pinMode(potPin,INPUT);
+   
+   pinMode(limitSwitchPin,INPUT);
+   
+   pinMode(encoderAPin,INPUT);
+   pinMode(encoderBPin,INPUT);
+   
+   pinMode(DCMotorPinA,OUTPUT);
+   pinMode(DCMotorPinB,OUTPUT);
+   
+   pinMode(IRPin,INPUT);
+   
+   pinMode(flexPin,INPUT);
+   pinMode(servoPin,OUTPUT);
+   
+   pinMode(stepPin,OUTPUT);
+   pinMode(dirPin,OUTPUT);
+   
 }
 
 void loop() {
   //debounce handled in hardware 
-  if(digitalRead(limitSwitchPin) == HIGH){
-    state = (!state) & 0x01; 
+  if(digitalRead(limitSwitchPin) == HIGH && button == LOW){
+    state = (state+1)%4; 
   }
+  button = digitalRead(limitSwitchPin);
   
-  //(state == 1) imples Sensor Mode
-  if(state == 1){  
   
-    //DC Motor Controlled by Potiometer and encoder
-    encoderAPrev = encoderAValue;
-    encoderAValue = digitalRead(encoderAPin);
-    
-    encoderBPrev = encoderBValue;
-    encoderBValue = digitalRead(encoderBPin);
-    
-    if(encoderAPrev != encoderAValue || encoderBPrev != encoderBValue){
-      DCMotorAngle = (DCMotorAngle+4)%360;
-    }
-    
-    potAngle = analogRead(potPin)*5*360/1024;
-    if(potAngle < DCMotorAngle - 4){
-      digitalWrite(DCMotorPinA,LOW);
-      digitalWrite(DCMotorPinB,HIGH);
-    }else if(potAngle > DCMotorAngle + 4){
-      digitalWrite(DCMotorPinA,HIGH);
-      digitalWrite(DCMotorPinB,LOW);
-    }else{
+  /* 
+  switch(state){
+    case OFF_STATE:
       digitalWrite(DCMotorPinA,LOW);
       digitalWrite(DCMotorPinB,LOW);
-    }    
-    
-    //Stepper Motor controlled by IR sensor
-    IRDistance = 20/((float)analogRead(IRPin)); //approx distance in cm
-    if(stepDistance < IRDistance - 1){
-       digitalWrite(dirPin, HIGH);
-       toggle = (!toggle) & 0x01;
-    }else if(stepDistance > IRDistance + 1){
-      digitalWrite(dirPin, LOW);
-      toggle = (!toggle) & 0x01;
-    }
-    
-    //Servo Controlled by flex sensor
-    analogWrite(servoPin,analogRead(flexPin)-1.4)*4.4);
-  }  
-  
-  delay(10);                   
+      break;
+    case SERVO_STATE:
+      //Servo Controlled by flex sensor
+      analogWrite(servoPin,(analogRead(flexPin)-1.4)*4.4);
+      digitalWrite(DCMotorPinA,LOW);
+      digitalWrite(DCMotorPinB,LOW);
+      break;
+    case DC_STATE:
+       //DC Motor Controlled by Potiometer and encoder
+      encoderAPrev = encoderAValue;
+      encoderAValue = digitalRead(encoderAPin);
+      
+      encoderBPrev = encoderBValue;
+      encoderBValue = digitalRead(encoderBPin);
+      
+      if(encoderAPrev != encoderAValue || encoderBPrev != encoderBValue){
+        DCMotorAngle = (DCMotorAngle+4)%360;
+      }
+      
+      potAngle = analogRead(potPin)*5*360/1024;
+      if(potAngle < DCMotorAngle - 4){
+        digitalWrite(DCMotorPinA,LOW);
+        digitalWrite(DCMotorPinB,HIGH);
+      }else if(potAngle > DCMotorAngle + 4){
+        digitalWrite(DCMotorPinA,HIGH);
+        digitalWrite(DCMotorPinB,LOW);
+      }else{
+        digitalWrite(DCMotorPinA,LOW);
+        digitalWrite(DCMotorPinB,LOW);
+      }    
+      break;
+    case STEP_STATE:
+      //Stepper Motor controlled by IR sensor
+      IRDistance = 20/((float)analogRead(IRPin)); //approx distance in cm
+      if(stepDistance < IRDistance - 1){
+         digitalWrite(dirPin, HIGH);
+         toggle = (!toggle) & 0x01;
+      }else if(stepDistance > IRDistance + 1){
+        digitalWrite(dirPin, LOW);
+        toggle = (!toggle) & 0x01;
+      }
+      digitalWrite(DCMotorPinA,LOW);
+      digitalWrite(DCMotorPinB,LOW);
+      break;
+    default:
+     Serial.write("Error: unknown state");
+     digitalWrite(DCMotorPinA,LOW);
+     digitalWrite(DCMotorPinB,LOW);
+     delay(10000);
+     break; 
+  }
+*/    
+  delay(500);                   
   Serial.flush();
 }
 
