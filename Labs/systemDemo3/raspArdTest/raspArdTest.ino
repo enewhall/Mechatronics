009abd10d8fa2d3ddda15cc35c8c901557ae470a
enewhall <ewnewhal@gmail.com>
2015-03-17 16:55:05 -0400

    improvements to LED Orientation Display code

--- Labs/systemDemo3/raspArdTest/raspArdTest.ino
+++ Labs/systemDemo3/raspArdTest/raspArdTest.ino
@@ -1,9 +1,18 @@
-const int ledPin = 13;
+const int orient0 = 13;
+const int orient1 = 12;
+const int orient2 = 11;
+const int orient3 = 10;
+const int orient4 = 9;
 
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  pinMode(orient0, OUTPUT);
+  pinMode(orient1, OUTPUT);
+  pinMode(orient2, OUTPUT);
+  pinMode(orient3, OUTPUT);
+  pinMode(orient4, OUTPUT);
+  
   Serial.begin(9600);
 }
 void loop()
@@ -11,10 +20,47 @@ void loop()
   if (Serial.available()) {
     switch(Serial.read()){
       case '0':
-        digitalWrite(ledPin, LOW);
+        digitalWrite(orient0, HIGH);
+        digitalWrite(orient1, LOW);
+        digitalWrite(orient2, LOW);
+        digitalWrite(orient3, LOW);
+        digitalWrite(orient4, LOW);
         break;
+      case '1':
+        digitalWrite(orient0, LOW);
+        digitalWrite(orient1, HIGH);
+        digitalWrite(orient2, LOW);
+        digitalWrite(orient3, LOW);
+        digitalWrite(orient4, LOW);
+        break;
+      case '2':
+        digitalWrite(orient0, LOW);
+        digitalWrite(orient1, LOW);
+        digitalWrite(orient2, HIGH);
+        digitalWrite(orient3, LOW);
+        digitalWrite(orient4, LOW);
+        break;
+      case '3':
+        digitalWrite(orient0, LOW);
+        digitalWrite(orient1, LOW);
+        digitalWrite(orient2, LOW);
+        digitalWrite(orient3, HIGH);
+        digitalWrite(orient4, LOW);
+        break;
+      case '4':
+        digitalWrite(orient0, LOW);
+        digitalWrite(orient1, LOW);
+        digitalWrite(orient2, LOW);
+        digitalWrite(orient3, LOW);
+        digitalWrite(orient4, HIGH);
+        break; 
       default:
-        digitalWrite(ledPin, HIGH);
+        digitalWrite(orient0, LOW);
+        digitalWrite(orient1, LOW);
+        digitalWrite(orient2, LOW);
+        digitalWrite(orient3, LOW);
+        digitalWrite(orient4, LOW);
+        break;
     }
   }
 }
